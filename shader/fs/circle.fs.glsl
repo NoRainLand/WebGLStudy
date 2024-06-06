1568afa5dc7620f94285b2b52b12b28e8a133725
NoRain <wuxinrufeng@qq.com>
2024-06-06 20:09:57 +0800

    添加圆形

--- shader/fs/circle.fs.glsl
+++ shader/fs/circle.fs.glsl
@@ -13,11 +13,13 @@ void main() {
     vec2 uv = gl_FragCoord.xy / u_resolution.xy;
     float r = 0.4;
 
-    // float dis = sqrt(pow(abs(uv.x - 0.5), 2.) + pow(abs(uv.y - 0.5), 2.));
     float dis = distance(uv, vec2(0.5));
     if(dis < 0.4) {
         vec3 disColor = vec3(dis);
         gl_FragColor = vec4(disColor, 1.);
     }
+
+    //下面的性能会好一点 开方性能差
+    // float dis = pow(abs(uv.x - 0.5), 2.) + pow(abs(uv.y - 0.5), 2.);
 
 }
